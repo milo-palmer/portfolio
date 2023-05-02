33bcf46147ce7882384a525aa9cc53098630116d
Milo Palmer <milogpalmer@gmail.com>
2023-05-02 13:34:21 +1200

    images in public

--- client/components/Projects.tsx
+++ client/components/Projects.tsx
@@ -4,13 +4,14 @@ function Projects() {
       <div>
         <h2>Sims Scribble 98</h2>
         <p>
-          Is a fun drawing game similar to Telephone where users can draw and
-          caption drawings from their friends in a telephone secrecy style of
-          game
+          Is a fun drawing game similar to Telephone where users take turns
+          secretly draw and writing captions for drawings from their friends and
+          then get shown the results of the drawings and captions to see how
+          wacky and weird it gets
         </p>
       </div>
       <div>
-        <img src="" alt="Sims Scrabble home page" />
+        <img src="images/sims.png" alt="Sims Scrabble home page" />
       </div>
     </section>
   )
